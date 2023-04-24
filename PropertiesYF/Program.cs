e90e765bed779e996217eabdb6c043554eaa72f9
Yonayv <fixicoyonayv@gmail.com>
2023-04-24 13:03:44 -0500

    Initial commit

---
+++ PropertiesYF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertiesYF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
