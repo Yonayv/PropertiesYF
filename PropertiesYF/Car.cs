00d79730fd916901ccb4a2945fe45fa661cdbf35
Yonayv <fixicoyonayv@gmail.com>
2023-04-24 13:44:45 -0500

    Yiippie

---
+++ PropertiesYF/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesYF
+{
+   
+    /// <summary>
+    /// Car is our class that we access.
+    /// We access our model by applying private and public string
+    /// Get and set is used to return the model to its set value
+    /// The public string make was made public so we could access this code in our program
+    /// </summary>
+    class Car
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+    }
+}
--- PropertiesYF/Program.cs
+++ PropertiesYF/Program.cs
@@ -4,9 +4,30 @@ namespace PropertiesYF
 {
     class Program
     {
+
+        /// <summary>
+        /// each Car is created with a new car object which allows us to access the Car.ss
+        /// The car is assgigned to its make, where you can set the value.
+        /// each car value has its own unique set value
+        /// the console.writeline projects the set value back to the Car.se
+        /// </summary>
+        /// <param name="args"> </param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            Car car3 = new Car();
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
         }
     }
 }
